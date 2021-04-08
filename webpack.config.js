878813f1c6a05465df447388508bc5b5cb9996d1
Stanislav Panchenko <stanip@hotmail.de>
2021-04-08 16:02:21 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const cones = require('./cones.json');
+
+const getWebpackConfig = (config) => require(`./webpack.config.${config.env}`)(config)
+
+module.exports = (config) => {
+  console.log(config)
+  // console.log(getWebpackConfig(config))
+  return {
+    mode: 'development',
+    entry: './src/index',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'build')
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new CopyWebpackPlugin([{ from: 'public/index.html' }])
+    ],
+    devServer: {
+      port: 3000,
+      contentBase: path.join(__dirname, './public'),
+      hot: true,
+      open: true,
+      historyApiFallback: true,
+      before: function(app) {
+        app.get('/api/cones', function(req, res) {
+          res.json(cones);
+        });
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /.jsx?$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        }
+      ]
+    }
+  }
+};
